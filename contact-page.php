25969eecc6d794d5213976a68a9f3291711e522a
Arianna Delin <arianna.delin11@ufl.edu>
2016-04-07 21:13:02 -0400

    added contact page

---
+++ contact-page.php
@@ -0,0 +1,18 @@
+<?php
+/*Template Name: Contact Page
+*/
+?>
+<?php get_header(); ?>
+
+	<div class="row">
+		<div class="nine columns">
+			<?php if (have_posts()) :
+					while (have_posts()) : the_post();?>
+						<h2> <?php the_title (); ?></h2>
+						<?php the_content();
+					endwhile;
+				endif; ?>
+		</div>	
+	</div>	
+
+<?php get_footer(); ?>
