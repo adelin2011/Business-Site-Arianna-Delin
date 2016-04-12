d5d72b1c0c2cde3d67705226eab1e8d740b526c6
Arianna Delin <arianna.delin11@ufl.edu>
2016-04-11 22:10:35 -0400

    added styling to site title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <div class="row">
 	<div class="twelve columns">
 		<div id="cover_image">
-			<img src="<?php echo get_template_directory_uri() .'Img/haulover pic.JPG'; ?>" alt="cover_image">
+			<img src="<?php echo get_template_directory_uri() .'Img/haulover.jpg'; ?>" alt="cover_image">
 		</div>	
 	</div>
 </div>
