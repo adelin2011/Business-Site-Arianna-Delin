5b4c1faa4c898397c3158707c6a9dde59de6f34c
Arianna Delin <arianna.delin11@ufl.edu>
2016-04-13 21:14:42 -0400

    added menu to header

--- header.php
+++ header.php
@@ -10,36 +10,19 @@
 <body>
 <div class="header-wrapper">
 	<div class="container">
-		<header class="row">
+		<header class="row header">
 			<div class="three columns"> 
 				<figure class="site-logo">
 					<a href="http://aridelin.com/WIE/"><img src="<?php echo get_template_directory_uri() . '/Img/Logo2.png'; ?>" width="100%" height="auto"> </a>	
 				</figure>
 			</div> 
 				<div class="nine columns">
-					<nav class="navigation">
-						<ul>
-							<li class="current_page"> <a href="index.html"> HOME </a> </li>
-							<li> <a href="about.html"> ABOUT </a> </li>
-							<li> <a href="services.html"> SERVICES </a> </li>
-							<li> <a href="blog.html"> BLOG </a> </li>
-							<li> <a href="contact.html"> CONTACT </a> </li>
-						</ul>
-					</nav>
+					<?php wp_nav_menu(array(
+	            		'sort_column' => 'menu_order', 
+	            		'container_class' => 'austins-menu-header'
+	            	));?>
 				</div>	
 		</header>
-		
-		<!--Wordpress menu
-		<div class="row">
-	    	<div class="five columns">
-	        	<?php wp_nav_menu(array(
-	            	'sort_column' => 'menu_order', 
-	            	'container_class' => 'navigation'
-	            	));?>
-	    	</div>
-		</div>
-		-->
-		
 	</div>
 </div>	
 
