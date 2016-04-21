dec57b797cddec42a633fb0fe98d1fc517f16bd6
Arianna Delin <arianna.delin11@ufl.edu>
2016-04-21 10:03:44 -0400

    added slider file

---
+++ slider.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Slider
+*/
+?>
+<?php get_header(); ?>
+	<div class="container" id="background">
+		<div class="row">	
+			<div class="twelve columns">
+				<div class="my-slider">
+				    <ul>
+				        <li style="background-image: url('slide01.jpg');">
+				            <div class="slides-message">
+				                <h1>Slide One Title</h1>
+				                <p>This is where the excerpt for the slides will go</p>
+				            </div>
+				        </li>
+				        <li style="background-image: url('slide02.jpg');">
+				            <div class="slides-message">
+				                <h1>Slide Two Title</h1>
+				                <p>This is where the excerpt for the slides will go</p>
+				            </div>
+				        </li>
+				        <li style="background-image: url('slide03.jpg');">
+				            <div class="slides-message">
+				                <h1>Slide Three Title</h1>
+				                <p>This is where the excerpt for the slides will go</p>
+				            </div>
+				        </li>
+				    </ul>  
+ 				</div> 
+ 			</div> 	
+ 		</div> 	
+	</div>
+			
+<?php get_footer(); ?>
