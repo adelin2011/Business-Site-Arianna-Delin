aa110f612525a1f7f3a635e538dd33f146a9fff3
Arianna Delin <arianna.delin11@ufl.edu>
2016-04-25 13:22:04 -0400

    added gallery page

---
+++ gallery.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Gallery
+*/
+?>
+<?php get_header(); ?>
+	<div class="container" id="background">
+		<div class="row">	
+			<div class="twelve columns">
+				<div class="wrap">
+			        <ul id="gallery">
+			            <li class="photos"><a href=""><img src="<?php echo get_template_directory_uri() . '/img/greenwich.jpg'; ?>" width="100%" height="auto">
+			            <li class="photos"><a href=""><img src="<?php echo get_template_directory_uri() . '/img/website1.JPG'; ?>" width="100%" height="auto">
+			            <li class="photos"><a href=""><img src="<?php echo get_template_directory_uri() . '/img/tracks.jpg'; ?>" width="100%" height="auto">
+			            <li class="photos"><a href=""><img src="<?php echo get_template_directory_uri() . '/img/audi2.jpg'; ?>" width="100%" height="auto">
+			            <li class="photos"><a href=""><img src="<?php echo get_template_directory_uri() . '/img/overpass.jpg'; ?>" width="100%" height="auto">
+			            <li class="photos"><a href=""><img src="<?php echo get_template_directory_uri() . '/img/beach2.jpg'; ?>" width="100%" height="auto">
+			        </ul>
+    			</div>
+ 			</div> 	
+ 		</div> 	
+	</div>
+
+
+			
+<?php get_footer(); ?>
+
+
